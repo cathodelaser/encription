6b52afb1b0438ad5a59f73b2b4ea9522cfdf0a6c
Griffin <48079968+cathodelaser@users.noreply.github.com>
2019-03-18 00:42:45 +0800

    Update main.c

--- main.c
+++ main.c
@@ -11,26 +11,40 @@ const char keybook[ROWS][COLS] = {
 	{'7', '1', '3', '4', '8', '2', '5', '6', '0', '9'}
 };
 char input[SENT];
-int count = 0;
+int input_count = 0;
 
 /*
 function declarations
 */
-void code(char);
-char get_fir(void);
-char get_fir_char(void);
+extern void code(char);
+extern char get_fir(void);
+extern char get_fir_char(void);
+extern int s_gets(char *, int);
+extern int remain(int);
+extern int codcmp(char *);
+extern void pcode(int, int, int);
+extern int EVEN(int);
+extern int codecat(int);
+extern void construct(int);
 
 int main(void)
 {
 	char cmd;
+	auto int count;
 
+	/*
+	Enter input, and toupper it
+	*/
 	printf("Please make your input.\n");
-	fgets(input, SENT, stdin);
-	for (count = 0; count < SENT; count++)
+	input_count = s_gets(input, SENT);
+
+	for (count = 0; count < input_count; count++)
 		input[count] = toupper(input[count]);
 
-	printf("Do you want to encript this pharse or decypher it?\n");
-	printf("(E\C. encript   D. decypher   Q. quit): ");
+	printf("You entered %d characters.\n", input_count);
+
+	printf("Do you want to encrypt this pharse or decypher it?\n");
+	printf("(E\C. encrypt   D. decypher   Q. quit): ");
 	cmd = get_fir_char();
 	cmd = toupper(cmd);
 
@@ -42,7 +56,7 @@ int main(void)
 			getch();
 
 			printf("Any further command?\n");
-			printf("(E\C. encript   D. decypher   Q. quit): ");
+			printf("(E\C. encrypt   D. decypher   Q. quit): ");
 			cmd = get_fir_char();
 			cmd = toupper(cmd);
 		}
@@ -54,35 +68,7 @@ int main(void)
 		}
 	}
 
-	puts("Your code is: ");
-	puts(input[SENT]);
+	printf("Your code is:\n%s", input);
 
 	return 0;
-}
-
-char get_fir(void)
-{
-	char ch;
-	ch = getchar();
-
-	while (getchar() != '\n')
-		continue;
-
-	return ch;
-}
-
-char get_fir_char(void)
-{
-	char ch;
-	ch = getchar();
-
-	while (ch < 'A' || ch > 'z' || (ch > 'Z' && ch < 'a'))
-	{
-		ch = getchar();
-	}
-
-	while (getchar() != '\n')
-		continue;
-
-	return ch;
 }
